d79bc15af02c3550011028e8919661f86e49ff9f
jneo8 <jneo8lin@gmail.com>
2024-07-17 23:37:58 +0800

    feat: Change the way to get api connection

--- internal/juju/api.go
+++ internal/juju/api.go
@@ -1,31 +1,64 @@
 package juju
 
 import (
+	"errors"
+
 	"github.com/juju/juju/api"
-	"github.com/juju/names/v5"
+	"github.com/juju/juju/cmd/modelcmd"
+	"github.com/juju/juju/juju"
+	"github.com/juju/juju/jujuclient"
 )
 
-func (jc *jujuClient) GetRootAPI(controllerName string) (api.Connection, error) {
-	ctrl, err := jc.clientStore.ControllerByName(controllerName)
-	if err != nil {
-		return nil, err
-	}
-
+func (jc *jujuClient) GetRootAPI(controllerName string, modelName string) (api.Connection, error) {
 	accountDetails, err := jc.clientStore.AccountDetails(controllerName)
 	if err != nil {
 		return nil, err
 	}
-
-	info := api.Info{
-		Addrs:          ctrl.APIEndpoints,
-		CACert:         ctrl.CACert,
-		ControllerUUID: ctrl.ControllerUUID,
-		Password:       accountDetails.Password,
-		Tag:            names.NewUserTag(accountDetails.User),
+	newAPIConnectionParams, err := jc.GetNewAPIConnectionParams(
+		jc.clientStore,
+		controllerName,
+		modelName,
+		accountDetails,
+		api.Open,
+	)
+	if err != nil {
+		return nil, err
 	}
-	apiRoot, err := api.Open(&info, api.DefaultDialOpts())
+
+	apiRoot, err := juju.NewAPIConnection(newAPIConnectionParams)
 	if err != nil {
 		return nil, err
 	}
 	return apiRoot, nil
+}
+
+var errNoNameSpecified = errors.New("no name specified")
+
+func (jc *jujuClient) GetNewAPIConnectionParams(
+	store jujuclient.ClientStore,
+	controllerName string,
+	modelName string,
+	accountDetails *jujuclient.AccountDetails,
+	apiOpen api.OpenFunc,
+) (juju.NewAPIConnectionParams, error) {
+	if controllerName == "" {
+		return juju.NewAPIConnectionParams{}, errNoNameSpecified
+	}
+	var modelUUID string
+	if modelName != "" {
+		modelDetails, err := store.ModelByName(controllerName, modelName)
+		if err != nil {
+			return juju.NewAPIConnectionParams{}, err
+		}
+		modelUUID = modelDetails.ModelUUID
+	}
+	dialOpts := api.DefaultDialOpts()
+	return juju.NewAPIConnectionParams{
+		Store:          store,
+		ControllerName: controllerName,
+		AccountDetails: accountDetails,
+		ModelUUID:      modelUUID,
+		DialOpts:       dialOpts,
+		OpenAPI:        modelcmd.OpenAPIFuncWithMacaroons(apiOpen, store, controllerName),
+	}, nil
 }
